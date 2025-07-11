0e22d90afb50b43d710feee40be85d5e16e48549
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2025-07-11 19:51:07 +0200

    feat: Initial version of the quote application

---
+++ quote.py
@@ -0,0 +1,7 @@
+# quote.py
+def main():
+    """Displays a static, inspirational quote."""
+    print("Socrates: 'The only true wisdom is in knowing you know nothing.'")
+
+if __name__ == "__main__":
+    main()
